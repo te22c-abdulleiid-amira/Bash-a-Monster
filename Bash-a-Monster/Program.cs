34ea2e80e49c709decab940cb5454109285508de
Amira Abdulle Iid <amira.abdulleiid@elev.ga.nitg.se>
2024-09-09 19:30:22 +0200

    first commit

---
+++ Bash-a-Monster/Program.cs
@@ -0,0 +1,19 @@
+﻿// skapar ett nytt objekt av klassen Giorno och monster och tilldelar det variabeln theGiorno/theMonster
+Giorno theGiorno = new Giorno();
+Monster theMonster = new Monster();
+
+// skriver ut deras namn och hälsopoäng (hp) till konsolen
+Console.WriteLine($"{theGiorno.name} har {theGiorno.health} hp");
+Console.WriteLine($"{theMonster.name} har {theMonster.health} hp och {theMonster.size} storlek");
+
+// skriver ut "fight" till konsolen för att indikera att en strid börjar
+Console.WriteLine("fight");
+
+// minskar Monstrets hälsopoäng (hp) med 10
+theMonster.health -= 10;
+
+// skriver ut Monstrets namn och nuvarande hälsopoäng (hp) till konsolen efter skadan
+Console.WriteLine($"{theMonster.name} har nu {theMonster.health} hp");
+
+Console.WriteLine("tryck vart som helst för att fortsätta");
+Console.ReadKey();
---
+++ Bash-a-Monster/giorno.cs
@@ -0,0 +1,14 @@
+using System;
+
+// Definierar en ny klass kallad "Giorno"
+public class Giorno
+{
+  // Deklarerar en publik heltalsvariabel (int) som representerar Giornos hälsopoäng (hp), och sätter dess startvärde till 100
+  public int health = 100;
+
+  // Deklarerar en publik heltalsvariabel (int) som representerar Giornos attackstyrka, och sätter dess startvärde till 10
+  public int attack = 10;
+
+  // Deklarerar en publik strängvariabel (string) som representerar Giornos namn, och sätter dess startvärde till "Giorno"
+  public string name = "Giorno";
+}
